eb27aab7aee09d838f4ececefc3c10c61361bcc0
Pierre RAMBAUD <pierre.rambaud@prestashop.com>
2018-12-26 15:05:23 +0100

    Example lib logs

--- tests-legacy/check_file_syntax.sh
+++ tests-legacy/check_file_syntax.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+readonly BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/"
+source ${BASE_DIR}/lib/log.sh
+
 ! (find . -name "*.php" ! -path "./vendor/*" ! -path "./tools/*" -print0 | xargs -0 -n1 -P4 php -l | grep -q "Parse error")
 php=$?
 
@@ -21,9 +24,9 @@ php ./vendor/bin/php-cs-fixer fix --dry-run --stop-on-violation --show-progress=
 coding_styles=$?
 
 if [[ "$php" == "0" && "$yaml_src" == "0" && "$yaml_app" == "0" && "$yaml_themes" == "0" && "$yaml_trad" == "0" && "$coding_styles" == "0" ]]; then
-  echo -e "\e[92mSYNTAX TESTS OK"
+  log_sucess 'SYNTAX TESTS OK'
   exit 0;
 else
-  echo -e "\e[91mSYNTAX TESTS FAILED"
+  log_error 'SYNTAX TESTS FAILED'
   exit 255;
 fi
---
+++ tests-legacy/lib/log.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+function log_warn() {
+    printf "\e[93m[WARN] %s\e[39m\n" "$@" >&2
+}
+
+function log_error() {
+    printf "\e[91m[ERROR] %s\e[39m\n" "$@" >&2
+}
+
+function log_info() {
+    printf "\e[94m[INFO] %s\e[39m\n" "$@" >&2
+}
+
+function log_success() {
+    printf "\e[32m[SUCCESS] %s\e[39m\n" "$@" >&2
+}
